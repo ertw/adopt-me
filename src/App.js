39959404397404337dbedf55b068e4be89b80357
Erik Williamson <me@erik.tw>
2024-02-06 08:37:31 -0300

    use props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-const Pet = () => {
+const Pet = ({name, animal, breed}) => {
     return React.createElement('div', {key: 1}, [
-        React.createElement('h1', {key: 2}, 'Luna'),
-        React.createElement('h2', {key: 3}, 'Dog'),
-        React.createElement('h2', {key: 4}, 'Havanese'),
+        React.createElement('h1', {key: 2}, name),
+        React.createElement('h2', {key: 3}, animal),
+        React.createElement('h2', {key: 4}, breed),
     ]);
 }
 const App = () => {
     return React.createElement('div', {className: "react-root"}, [
         React.createElement('h1', {key: 1}, 'Adopt Me!'),
         React.createElement('h2', {key: 2}, 'Welcome to Adopt Me!'),
-        React.createElement(Pet, {key: 3}),
-        React.createElement(Pet, {key: 4}),
-        React.createElement(Pet, {key: 5}),
+        React.createElement(Pet, {
+            key: 3,
+            name: "Luna",
+            animal: "Dog",
+            breed: "Havanese"
+        }),
+        React.createElement(Pet, {
+            key: 4,
+            name: "Pepper",
+            animal: "Bird",
+            breed: "Cockatiel"
+        }),
+        React.createElement(Pet, {
+            key: 5,
+            name: "Doink",
+            animal: "Cat",
+            breed: "Mixed"
+        }),
     ]);
 };
 
