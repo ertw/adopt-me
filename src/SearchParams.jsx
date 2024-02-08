ee646ba610900631e19f54e2959b6d452d93e470
Erik Williamson <me@erik.tw>
2024-02-08 11:41:48 -0300

    Make api request

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,15 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Pet from "./Pet.jsx";
 
 const SearchParams = () => {
   const animals = ["", "bird", "cat", "dog", "rabbit", "reptile"];
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
   const breeds = [];
   const [location, setLocation] = useState("");
 
+  useEffect(() => {
+    requestPets();
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`,
+    );
+    const json = await res.json();
+    setPets(json.pets);
+  }
+
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -54,6 +73,14 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      {pets.map((pet) => (
+        <Pet
+          animal={pet.animal}
+          key={pet.id}
+          name={pet.name}
+          breed={pet.breed}
+        />
+      ))}
     </div>
   );
 };
