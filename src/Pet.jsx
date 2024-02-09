02636586eac40d1a7fac14b52593857484e2d092
Erik Williamson <me@erik.tw>
2024-02-09 08:54:35 -0300

    Get pet images

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -8,6 +8,9 @@ import React from "react";
  * @property {string} name - The name of the pet.
  * @property {string} animal - The type of animal the pet is.
  * @property {string} breed - The breed of the pet.
+ * @property {string[]} images - The images of the pet.
+ * @property {string} location - The location of the pet.
+ * @property {string} id - The id of the pet.
  */
 /**
  * Pet component in React.
@@ -17,13 +20,21 @@ import React from "react";
  * @returns {JSX.Element} A React element that displays the pet's name, animal type, and breed.
  */
 
-const Pet = ({ name, animal, breed }) => {
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = `http://pets-images.dev-apis.com/pets/none.jpg`;
+  if (images.length) {
+    hero = images[0];
+  }
   return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   );
 };
 
